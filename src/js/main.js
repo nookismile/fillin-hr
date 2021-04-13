2a93372ecdf77d5c022e76df86eac338e6c18532
nookismile <stina.novickova@yandex.ru>
2021-04-13 10:31:25 +0300

    add main page

--- src/js/main.js
+++ src/js/main.js
@@ -2,20 +2,20 @@ $(document).ready(function () {
 
 // Modal
 
-// $(".js-modal").on("click", function (e) {
-//     e.preventDefault();
-//     var currentModal = $(this).attr("href");
-//     $(currentModal + ", #js-overlay").fadeIn(500);
-//     $("body").addClass("open-modal");
+$(".js-modal").on("click", function (e) {
+    e.preventDefault();
+    var currentModal = $(this).attr("href");
+    $(currentModal + ", #js-overlay").fadeIn(500);
+    $("body").addClass("open-modal");
 
-//   });
+  });
 
 
-//   $("#js-overlay, .js-modal-close").on("click", function (e) {
-//     e.preventDefault();
-//     $(".modal, #js-overlay").fadeOut(100);
-//     $("body").removeClass("open-modal");
-//   });
+  $("#js-overlay, .js-modal-close").on("click", function (e) {
+    e.preventDefault();
+    $(".modal, #js-overlay").fadeOut(100);
+    $("body").removeClass("open-modal");
+  });
 
 
   $(".choice__link").on("click", function (e) {
@@ -29,6 +29,41 @@ $(document).ready(function () {
     e.preventDefault();
     $(".country__window, #js-overlay").fadeOut(100);
     $("body").removeClass("open-modal");
+  });
+
+
+  let swiper = new Swiper('.slider-app',{
+    loop: true,
+    centeredSlides: true,
+    loopFillGroupWithBlank: true,
+    slidesPerView: true,
+    initialSlide: 3,
+    keyboardControl: true,
+    mousewheelControl: false,
+    lazyLoading: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        1199: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        767: {
+            slidesPerView: 2,
+            spaceBetween: 1,
+        },
+        575: {
+            slidesPerView: 1,
+            spaceBetween: 1,
+        },
+    }
+
   });
   
 
