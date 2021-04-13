3c99b68697e649ae187442d4ce0bbf6c4d7f20c2
nookismile <stina.novickova@yandex.ru>
2021-04-13 13:23:50 +0300

    add main page without swipe-close and country modals

--- src/js/main.js
+++ src/js/main.js
@@ -68,3 +68,48 @@ $(".js-modal").on("click", function (e) {
   
 
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+const pushmenu = document.getElementsByClassName('swipe');
+const swipeClose = document.getElementsByClassName('swipe-close');
+
+	// получаем элемент с классом hidden-overley
+	const hiddenOverley = document.querySelector('.hidden-overley');
+
+	// отслеживаем клик клика по оверлею
+	hiddenOverley.addEventListener('click', (e) => {
+		e.currentTarget.classList.toggle('show');
+		document.querySelector('.sidebar-menu').classList.toggle('show');
+		document.querySelector('body').classList.toggle('sidebar-opened');
+		for( i=0; i < pushmenu.length; i++ ){
+				pushmenu[i].classList.toggle('open');
+		}
+	});
+
+	const pushmenuFunction = function() {
+		document.querySelector('.swipe').classList.toggle('open');
+		document.querySelector('.sidebar-menu').classList.toggle('show');
+		document.querySelector('.hidden-overley').classList.toggle('show');
+		document.body.classList.toggle('sidebar-opened')
+	};
+
+	// Отслеживаем клики кнопок с классом pushmenu 
+	for( i=0; i < pushmenu.length; i++ ){
+		pushmenu[i].addEventListener('click', pushmenuFunction, false);
+	}
+
+	// Получим все родительские элементы в меню
+	const sidebarAccordeon = document.querySelectorAll('.sidebar-menu .menu-parent-item a:first-child');
+	const accordeonFunction =  function() { 
+		this.parentNode.querySelector('ul').classList.toggle('show');
+		this.querySelector('span').classList.toggle('rotate');
+	}
+	// Отслеживаем клики родительских пунктов меню 
+	for( i=0; i < sidebarAccordeon.length; i++ ){
+		sidebarAccordeon[i].addEventListener('click', accordeonFunction, false);
+	}
+
+
+
+});
